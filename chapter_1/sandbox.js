7b57d377c33bc1e8ade09fec38e9f5226a591188
WildeMedia <wildemedia33@gmail.com>
2020-03-19 11:52:02 +0000

    Initial commit

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,15 @@
+//  alert('hello Clarice, I love the cut of your gib');
+// console.log(1);
+// console.log(2);
+
+let age = 25;
+let year = 2020;
+
+console.log(age, year);
+
+age=30;
+console.log(age);
+
+const points=100;
+console.log(points);
+points = 50;

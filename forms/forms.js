5a83cca5ec4418afa5a4379bfd364874469373fb
WildeMedia <wildemedia33@gmail.com>
2020-03-29 13:19:41 +0100

    Added some basic form validation using a regular expression to check user input.

--- forms/forms.js
+++ forms/forms.js
@@ -1,11 +1,32 @@
+// // query a form
+// const form = document.querySelector('.signup-form');
+// // const username = document.querySelector('#username');
+
+// form.addEventListener('submit', e => {
+//   //stop the page refreshing on submit
+//   e.preventDefault();
+//   // console.log(username.value);
+//   // use dot notation to get the username value from the form, quicker than using a querySelector
+//   console.log(form.username.value);
+
+// });
+
+// form validation
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
-  //stop the page refreshing on submit
   e.preventDefault();
-  // console.log(username.value);
-  // use dot notation to get the username value from the form, quicker than using a querySelector
-  console.log(form.username.value);
+ 
+  //validation
+  const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
+  if (usernamePattern.test(username)){
+    // valid name
+    feedback.textContent = 'that will do pig'
+  } else{
+    // name not valid
+    feedback.textContent = 'computer says no, please enter letters only, between 6-10 characters'
+  }
 });

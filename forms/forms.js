d2f3678172f188f0968808f340481a7221f86d26
WildeMedia <wildemedia33@gmail.com>
2020-03-29 13:49:17 +0100

    Added some live form validation using the keyup event.

--- forms/forms.js
+++ forms/forms.js
@@ -14,13 +14,14 @@
 // form validation
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
  
   //validation
   const username = form.username.value;
-  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+ 
 
   if (usernamePattern.test(username)){
     // valid name
@@ -28,5 +29,18 @@ form.addEventListener('submit', e => {
   } else{
     // name not valid
     feedback.textContent = 'computer says no, please enter letters only, between 6-10 characters'
+  }
+});
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+  //get the value of the targeted event
+  // console.log(e.target.value) //best practice 
+  //or
+  // console.log(form.username.value);
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class', 'success')
+  } else {
+    form.username.setAttribute('class', 'error')
   }
 });

95bebe0fee27969f9d313ad0bb358c4836b26b94
WildeMedia <wildemedia33@gmail.com>
2020-03-30 15:14:35 +0100

    Used the map() method to create an array by calling a specific function on each element present in the parent array.

--- array_methods/sandbox.js
+++ array_methods/sandbox.js
@@ -1,4 +1,5 @@
-// use the filter() method on the array to return and omit items
+// filter method
+// use the filter() method to create a new array with all the elements that pass the test implemented by the callback() function.
 const scores = [10, 30, 15, 25, 40, 5];
 
 const filteredScores = scores.filter(score =>{
@@ -23,3 +24,32 @@ const folks = [
 const filterFolks = folks.filter(folk => folk.sorceress);
 
 console.log(filterFolks);
+
+// <----------------------------------------------------------------------------->
+
+// map method
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+// use the map() method to create an array by calling a specific function on each element present in the parent array. 
+
+const salesPrices = prices.map(price =>  price / 2);
+
+console.log(salesPrices);
+
+
+const fruits = [
+  {name: 'banana', price: 20},
+  {name: 'apple', price: 25},
+  {name: 'lime', price: 40},
+  {name: 'peach', price: 35},
+  {name: 'orange', price: 10}
+];
+
+const saleFruits = fruits.map(fruit => {
+  if(fruit.price > 30){
+    return {name: fruit.name, price: fruit.price / 2};
+  } else {
+    return fruit;
+  }
+});
+
+console.log(saleFruits);

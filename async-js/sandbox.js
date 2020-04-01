bc65e3c8ed4fade5367f96130512bcfce530146f
WildeMedia <wildemedia33@gmail.com>
2020-04-01 17:04:05 +0100

    added a check to verify if the status of the request is correct.

--- async-js/sandbox.js
+++ async-js/sandbox.js
@@ -1,9 +1,9 @@
 // asynchronous javascript
 
-// rudimentary example of async javascript in action
+//rudimentary example of async javascript in action
 console.log(1);
 console.log(2);
-// trigger a callback using setTimeOut
+//trigger a callback using setTimeOut
 setTimeout(() => {
   console.log('the callback function fired')
 }, 2000);
@@ -11,13 +11,15 @@ console.log(3);
 console.log(4);
 
 
-// made a HTTP request
+// made a HTTP request to the JSON Placeholder API
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-  // console.log(request, request.readyState);
-  if(request.readyState === 4){
+  //added a check to verify if the status of the request is correct, if not, return a message to inform that there is an error
+  if(request.readyState === 4 && request.status === 200){
     console.log(request.responseText);
+  } else if(request.readyState === 4){
+    console.log('could not fetch the data');
   }
 });
 

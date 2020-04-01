704e4326e2f7d9734ecd12c8896fb2dbbadfa984
WildeMedia <wildemedia33@gmail.com>
2020-04-01 16:50:17 +0100

    Made a HTTP request to the JSONPlaceholder API

--- async-js/sandbox.js
+++ async-js/sandbox.js
@@ -9,3 +9,18 @@ setTimeout(() => {
 }, 2000);
 console.log(3);
 console.log(4);
+
+
+// made a HTTP request
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+  // console.log(request, request.readyState);
+  if(request.readyState === 4){
+    console.log(request.responseText);
+  }
+});
+
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();

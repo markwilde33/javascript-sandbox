ff12f2335cd291bb32c77a635ae2c63d8c922257
WildeMedia <wildemedia33@gmail.com>
2020-04-06 15:56:52 +0100

    Delete items from local storage

--- local_storage/sandbox.js
+++ local_storage/sandbox.js
@@ -16,3 +16,11 @@ name=localStorage.getItem('name');
 age=localStorage.getItem('age');
 
 console.log([name, age]);
+
+
+// delete data from local storage
+// localStorage.removeItem('name');
+localStorage.clear();
+name=localStorage.getItem('name');
+age=localStorage.getItem('age');
+console.log(name);

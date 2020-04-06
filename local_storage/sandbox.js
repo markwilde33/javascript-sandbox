1d0d4b2b55f892ca9acbcb5437272ea0bfea80ec
WildeMedia <wildemedia33@gmail.com>
2020-04-06 15:50:27 +0100

    Gave examples of storing data to, getting data from, and updating data of local storage.

--- local_storage/sandbox.js
+++ local_storage/sandbox.js
@@ -1,9 +1,18 @@
 // store data in local storage
-
-
+localStorage.setItem('name', 'flipper')
+localStorage.setItem('age', 100)
 
 // get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
 
+console.log([name, age]);
 
 
 // update data
+localStorage.setItem('name', 'luigi');
+localStorage.age = '200';
+name=localStorage.getItem('name');
+age=localStorage.getItem('age');
+
+console.log([name, age]);

2d0c98a0436c960da70be70adf02a35fb9205fa5
WildeMedia <wildemedia33@gmail.com>
2020-04-08 13:14:36 +0100

    Added a function to get data from each recipe and output to the browser.

--- firebase_db/sandbox.js
+++ firebase_db/sandbox.js
@@ -1,10 +1,25 @@
-db.collection('recipes').get().then((snapshot) => {
-// when we have the data
-//cycle through the array and output the data of each item to console
-snapshot.docs.forEach(doc => {
-  console.log(doc.data());
-})
+//get a reference to the ul element
+const list = document.querySelector('ul');
 
+// function to get data from each recipe and output to the browser
+const addRecipe = (recipe) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+  <li>
+    <div>${recipe.title}</div>
+    <div><small>${time}</small></div>
+  </li>
+`;
+
+list.innerHTML += html;
+};
+
+db.collection('recipes').get().then(snapshot => {
+  // when we have the data
+  //cycle through the array and get the data for each recipe by passing doc.data() as an argument
+  snapshot.docs.forEach(doc => {
+    addRecipe(doc.data());
+  });
 }).catch(err => {
   console.log(err)
-});
+}); 

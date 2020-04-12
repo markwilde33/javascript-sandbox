f5d6f107be95ce87b2756d73830ea3b840d45ad9
WildeMedia <wildemedia33@gmail.com>
2020-04-12 15:30:35 +0100

    Added the neccessary files and folders needed to convert modern js to older js so that all browsers can read it.

---
+++ workflow/dist/assets/bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var greet = function greet(name) {
+  console.log("hello ".concat(name));
+};
+
+greet('mario');
+greet('yoshi');
+greet('booboo');
+greet('cherie');
+greet('cherie');
+greet('cherie');
+greet('cherie');
---
+++ workflow/src/index.js
@@ -0,0 +1,11 @@
+const greet = name => {
+  console.log(`hello ${name}`);
+};
+
+greet('mario');
+greet('yoshi');
+greet('booboo');
+greet('cherie');
+greet('cherie');
+greet('cherie');
+greet('cherie');
